4f8bfc42472f8322dc255336412d1230a1f7d8b1
Kulinich Danil <d.kulinich@g.nsu.ru>
2026-09-03 22:07:08 +0700

    Clear all

--- Task_1_1_1/src/main/java/ru/nsu/oop/drakonokot/Main.java
+++ Task_1_1_1/src/main/java/ru/nsu/oop/drakonokot/Main.java
@@ -1,14 +1,10 @@
 package ru.nsu.oop.drakonokot;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     static void main() {
 
-        IO.println(String.format("Hello and welcome!"));
 
-        for (int i = 1; i <= 5; i++) {
-            IO.println("i = " + i);
+
         }
     }
 }
