e66e72230518b360a291cdf04b2fd202012cda52
qDrakonokot <qDrakonokot@users.noreply.github.com>
2026-09-14 10:42:20 +0000

    Deploying to gh-pages from @ qDrakonokot/OOP@ff0cd3f0ed15d2c760e26ce36af2995ffe2021cf 🚀

---
+++ Task_1_1_2/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ru.nsu.oop"},{"l":"ru.nsu.oop.actor"},{"l":"ru.nsu.oop.game"},{"l":"ru.nsu.oop.model"},{"l":"ru.nsu.oop.view"}];updateSearchResults();
