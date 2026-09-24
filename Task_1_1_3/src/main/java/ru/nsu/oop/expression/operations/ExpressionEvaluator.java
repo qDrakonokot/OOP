d9e97f47a0560ea068fae2419a896a67989ddcb4
Kulinich Danil <d.kulinich@g.nsu.ru>
2026-09-25 00:53:25 +0700

    Maybe do this??

--- Task_1_1_3/src/main/java/ru/nsu/oop/expression/operations/ExpressionEvaluator.java
+++ Task_1_1_3/src/main/java/ru/nsu/oop/expression/operations/ExpressionEvaluator.java
@@ -1,5 +1,19 @@
 package ru.nsu.oop.expression.operations;
 
+import java.util.Map;
+import ru.nsu.oop.expression.bricks.Add;
+import ru.nsu.oop.expression.bricks.Expression;
+import ru.nsu.oop.expression.bricks.Number;
+import ru.nsu.oop.expression.bricks.Variable;
+
 public class ExpressionEvaluator {
 
+    public int evaluate(Expression expr, Map<String, Integer> vars) {
+        return switch (expr) {
+            case Number n -> n.getValue();
+            case Variable v -> vars.get(v.getName());
+            case Add a -> evaluate(a.getLeft(), vars) + evaluate(a.getRight(), vars);
+            // ...
+        };
+    }
 }
